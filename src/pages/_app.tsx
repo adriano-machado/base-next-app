bfafec501efc834b7d7aa07bb81bb034fac4f700
Adriano Machado <adrianoricardom@gmail.com>
2020-11-02 14:37:00 -0300

    fix(_app.tsx): pulei uma linha
    Pulei uma linha pra testar os triggers
    
    BREAKING CHANGE: Primeiro commit com husky

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,7 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
+
       <GlobalStyle />
     </ThemeProvider>
   )
